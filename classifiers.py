fe0c2a921ad820365223bfc8ac6a039cde52ba2e
Reid Kairalla <kairalla@princeton.edu>
2018-02-24 23:39:15 -0500

    added classifiers, TFIDF

--- classifiers.py
+++ classifiers.py
@@ -1,72 +1,83 @@
 import sklearn
 import sys
 from sklearn.naive_bayes import MultinomialNB
+from sklearn.feature_extraction.text import TfidfTransformer
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
 from sklearn import svm
 from sklearn import metrics
 
+
 def main(argv):
-	classifier = argv
+    classifier = argv
+    tfidf = TfidfTransformer(norm="l2",use_idf=True, smooth_idf=True, sublinear_tf=False)
+    if classifier == 'MultinomialNB':
+        clf = MultinomialNB()
+    elif classifier == 'SVM':
+        clf = svm.SVC()
+    elif classifier == "KNN":
+        clf = KNeighborsClassifier(n_neighbors=12)
+    elif classifier == "RandomForest":
+        clf = RandomForestClassifier(n_estimators=100)
+    else:
+        print "No such classifier"
+        return
 
-	if classifier == 'MultinomialNB':
-		clf = MultinomialNB()
-	else:
-		if classifier == 'SVM':
-			clf = svm.SVC()
-		else:
-			print "No such classifier"
-			return
+    f = open('data/out_bag_of_words_5.csv', 'r')
+    lines = f.readlines()
 
-	f = open('data/out_bag_of_words_5.csv', 'r')
-	lines = f.readlines()
+    freq = [0] * len(lines)
+    i = 0
+    for line in lines:
+        counts = line.split(',')
+        freq[i] = [0] * len(counts)
+        j = 0
+        for val in counts:
+            freq[i][j] = int(val)
+            j += 1
+        i += 1
 
-	freq = [0] * len(lines)
-	i = 0
-	for line in lines:
-		counts = line.split(',')
-		freq[i] = [0] * len(counts)
-		j = 0
-		for val in counts:
-			freq[i][j] = int(val)
-			j += 1
-		i += 1
+    tfidf.fit_transform(freq, y=None)
+    print "whew"
+    f = open('data/out_classes_5.txt', 'r')
+    lines = f.readlines()
 
-	f = open('data/out_classes_5.txt', 'r')
-	lines = f.readlines()
+    sentiments = [0] * len(lines)
+    i = 0
+    for line in lines:
+        sentiments[i] = int(line)
+        i += 1
+    # tfidf.fit(freq, sentiments)
+    clf.fit(freq, sentiments)
 
-	sentiments = [0] * len(lines)
-	i = 0
-	for line in lines:
-		sentiments[i] = int(line)
-		i += 1
+    f = open('data/test_bag_of_words_0.csv', 'r')
+    lines = f.readlines()
 
-	clf.fit(freq, sentiments)
+    test = [0] * len(lines)
+    i = 0
+    for line in lines:
+        counts = line.split(',')
+        test[i] = [0] * len(counts)
+        j = 0
+        for val in counts:
+            test[i][j] = int(val)
+            j += 1
+        i += 1
 
-	f = open('data/test_bag_of_words_0.csv', 'r')
-	lines = f.readlines()
+    tfidf.transform(test)
+    predicted = clf.predict(test)
 
-	test = [0]  * len(lines)
-	i = 0
-	for line in lines:
-		counts = line.split(',')
-		test[i] = [0] * len(counts)
-		j = 0
-		for val in counts:
-			test[i][j] = int(val)
-			j += 1
-		i += 1
+    f = open('data/test_classes_0.txt', 'r')
+    lines = f.readlines()
 
-	predicted = clf.predict(test)
+    results = [0] * len(lines)
+    i = 0
+    for line in lines:
+        results[i] = int(line)
+        i += 1
 
-	f = open('data/test_classes_0.txt', 'r')
-	lines = f.readlines()
+    print metrics.accuracy_score(results, predicted)
 
-	results = [0] * len(lines)
-	i = 0
-	for line in lines:
-		results[i] = int(line)
-		i += 1
-
-	print metrics.accuracy_score(results , predicted)
 
 if __name__ == "__main__":
-  main(sys.argv[1])
+    main(sys.argv[1])
