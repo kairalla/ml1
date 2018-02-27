942740b4c7022821f746ac9ddcdeeb85d75da010
kairalla <32149651+kairalla@users.noreply.github.com>
2018-02-27 01:22:06 -0500

    feature selection, super improves all classifiers
    :D

--- classifiers.py
+++ classifiers.py
@@ -10,18 +10,17 @@ from sklearn import svm
 from sklearn import metrics
 from sklearn.metrics import roc_curve, auc
 from sklearn.tree import DecisionTreeClassifier
-
+from sklearn.feature_selection import chi2
+from sklearn.feature_selection import SelectKBest
 def main(argv):
+    kBestFactor = .5
 	# Choose classifier
     classifier = argv
     tfidf = TfidfTransformer(norm="l2", use_idf=True, smooth_idf=True, sublinear_tf=False)
     if classifier == 'MultinomialNB':
         clf = MultinomialNB()
     elif classifier == 'SVM':
-        clf = svm.SVC(C=1.0, cache_size=200, class_weight=None, coef0=0.0,
-    decision_function_shape='ovr', degree=3, gamma='auto', kernel='rbf',
-    max_iter=-1, probability=True, random_state=None, shrinking=True,
-    tol=0.001, verbose=False)
+        clf = svm.SVC()
     elif classifier == "KNN":
         clf = KNeighborsClassifier(n_neighbors=13)
     elif classifier == "RF":
@@ -48,7 +47,6 @@ def main(argv):
         i += 1
 
     tfidf.fit_transform(freq, y=None)
-
     # Read in classes
     f = open('data/out_classes_5.txt', 'r')
     lines = f.readlines()
@@ -59,7 +57,11 @@ def main(argv):
         sentiments[i] = int(line)
         i += 1
     # Fit the data
-    clf.fit(freq, sentiments)
+    chi = SelectKBest(chi2, k=int(len(freq[0])*kBestFactor))
+    freq2 = chi.fit_transform(freq, sentiments)
+    support = chi.get_support()
+    # print support
+    clf.fit(freq2, sentiments)
 
     # Read in test bag of words, tfidf transform, and predict
     f = open('data/test_bag_of_words_0.csv', 'r')
@@ -69,14 +71,16 @@ def main(argv):
     i = 0
     for line in lines:
         counts = line.split(',')
-        test[i] = [0] * len(counts)
+        test[i] = [0] * int(len(counts)* kBestFactor)
         j = 0
+        sup = 0
         for val in counts:
-            test[i][j] = int(val)
-            j += 1
+            if support[sup]:
+                test[i][j] = int(val)
+                j += 1
+            sup += 1
         i += 1
 
-    tfidf.transform(test)
     predicted = clf.predict(test)
 
     # Read in test classes and measure accuracy
@@ -92,11 +96,10 @@ def main(argv):
     print metrics.accuracy_score(results, predicted)
 
     # Calculate ROC curve
-    predictedProb = clf.predict_proba(test)
     fpr = dict()
     tpr = dict()
     roc_auc = dict()
-    fpr, tpr, _ = roc_curve(results, predictedProb[:, 1])
+    fpr, tpr, _ = roc_curve(results, predicted)
     roc_auc = auc(fpr, tpr)
 
     # Plot ROC
